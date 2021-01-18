3857894eec0997c428bf17dddd8c526205f8c9cd
anil-438 <kancharla1990@gmail.com>
2021-01-18 22:55:21 +0530

    3rd commit

--- demo/src/main/java/com/example/demo/git_first_day.java
+++ demo/src/main/java/com/example/demo/git_first_day.java
@@ -2,6 +2,6 @@ package com.example.demo;
 
 public class git_first_day {
     public static void main(String[] args) {
-        System.out.println("Hi");
+        System.out.println("Hi Anu");
     }
 }
